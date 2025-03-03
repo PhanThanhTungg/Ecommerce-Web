2be329727cd5117d0770898db71d478f25b6d919
git of tung <phantungpt03@gmail.com>
2025-03-03 17:43:31 +0700

    17:43 03-03-2025 test

--- public/js/product.js
+++ public/js/product.js
@@ -39,59 +39,42 @@ for(const x of sizeLayout){
 
 
 
-//slide
-var speed = 5;
-var tab = document.getElementById("demo");
-var tab1 = document.getElementById("demo1");
-var tab2 = document.getElementById("demo2");
-tab2.innerHTML = tab1.innerHTML;
-function Marquee() {
-  if (tab2.offsetWidth - tab.scrollLeft <= 0)
-    tab.scrollLeft -= tab1.offsetWidth
-  else {
-    tab.scrollLeft++;
-  }
-}
-var MyMar = setInterval(Marquee, speed);
-tab.onmouseover = function () { clearInterval(MyMar) };
-tab.onmouseout = function () { MyMar = setInterval(Marquee, speed) };
-
-
-
 //size
-const buttonSize = document.querySelectorAll(".button-size")
-buttonSize[0].classList.add("button-size-active")
-buttonSize.forEach(item=>{
-  item.addEventListener("click", (e)=>{
-    const priceNew = item.getAttribute("priceNew")
-    const price = item.getAttribute("price")
-    const stock = item.getAttribute("stock")
-    
-    const priceNewData = document.querySelector(".inner-price-new .formatMoney")
-    let moneyN = Number(priceNew)
-    let moneyS = moneyN.toLocaleString('vi', {style : 'currency', currency : 'VND'})
-    priceNewData.innerHTML = `${moneyS}`
-
-    const priceData = document.querySelector(".inner-price-old")
-    moneyN = Number(price)
-    moneyS = moneyN.toLocaleString('vi', {style : 'currency', currency : 'VND'})
-    priceData.innerHTML = `${moneyS}`
-
-    const stockData = document.querySelector(".stock")
-    stockData.innerHTML = stock
-
-    item.classList.add("button-size-active")
-    buttonSize.forEach(item1 =>{
-      if(item1!=item){
-        item1.classList.remove("button-size-active")
-      }
-    })
-
-    const CartInput = document.querySelector(".formCart .sizeInput")
-    const stockInput = document.querySelector(".formCart .stockInput")
-    CartInput.setAttribute("value", item.getAttribute("id"))
-    stockInput.setAttribute("max", item.getAttribute("stock"))
-  })
-})
+// const buttonSize = document.querySelectorAll(".button-size")
+// if(buttonSize){
+//   buttonSize[0].classList.add("button-size-active")
+//   buttonSize.forEach(item=>{
+//     item.addEventListener("click", (e)=>{
+//       const priceNew = item.getAttribute("priceNew")
+//       const price = item.getAttribute("price")
+//       const stock = item.getAttribute("stock")
+      
+//       const priceNewData = document.querySelector(".inner-price-new .formatMoney")
+//       let moneyN = Number(priceNew)
+//       let moneyS = moneyN.toLocaleString('vi', {style : 'currency', currency : 'VND'})
+//       priceNewData.innerHTML = `${moneyS}`
+  
+//       const priceData = document.querySelector(".inner-price-old")
+//       moneyN = Number(price)
+//       moneyS = moneyN.toLocaleString('vi', {style : 'currency', currency : 'VND'})
+//       priceData.innerHTML = `${moneyS}`
+  
+//       const stockData = document.querySelector(".stock")
+//       stockData.innerHTML = stock
+  
+//       item.classList.add("button-size-active")
+//       buttonSize.forEach(item1 =>{
+//         if(item1!=item){
+//           item1.classList.remove("button-size-active")
+//         }
+//       })
+  
+//       const CartInput = document.querySelector(".formCart .sizeInput")
+//       const stockInput = document.querySelector(".formCart .stockInput")
+//       CartInput.setAttribute("value", item.getAttribute("id"))
+//       stockInput.setAttribute("max", item.getAttribute("stock"))
+//     })
+//   })
+// }
 
 
---
+++ public/js/rating.js
@@ -0,0 +1,8 @@
+const rating_wrappers = document.querySelectorAll(".product-item .inner-rating");
+if(rating_wrappers){
+  rating_wrappers.forEach(rating=>{
+    const ratingNumber = rating.dataset.rating;
+    const ratingInput = rating.querySelector(`input[value="${ratingNumber}"]`);
+    ratingInput.checked = true;
+  })
+}
