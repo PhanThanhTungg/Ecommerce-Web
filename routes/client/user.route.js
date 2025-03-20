301e3f77f3ec92b0d09cfe4a46975b56a32df787
git of tung <phantungpt03@gmail.com>
2025-03-20 23:32:51 +0700

    login by facebook

--- routes/client/user.route.js
+++ routes/client/user.route.js
@@ -49,6 +49,14 @@ router.get("/google/callback",
   controller.googleCallback
 );
 
+router.post("/login-facebook",passport.authenticate("facebook", { scope: ["email"] }))
+
+router.get(
+  "/facebook/callback",
+  passport.authenticate("facebook", { failureRedirect: "/login", session: false }),
+  controller.facebookCallback
+)
+
 router.get("/logout", controller.logout);
 
 router.get("/password/forgot", controller.forgotPassword)
