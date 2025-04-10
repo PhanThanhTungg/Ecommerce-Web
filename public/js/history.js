f4d0708e089e99c930af0da7c340d573a5fc8b1e
git of tung <phantungpt03@gmail.com>
2025-04-10 12:25:16 +0700

    feat: enhance order history page with QR code display and improved styling

--- public/js/history.js
+++ public/js/history.js
@@ -1,25 +1,26 @@
-// const paymentStatusSections = document.querySelectorAll(".paymentStatus-section[status='lack']")
+const paymentStatusSections = document.querySelectorAll(".paymentStatus-section[status='lack']")
 
-// if(paymentStatusSections){
-//   [...paymentStatusSections].forEach(item=>{
-//     fetch(`/api/checkout/delivery-status/qr/${item.dataset.orderId}`).then(res=>res.json())
-//       .then(data=>{
-//         console.log(data);
-//       }).catch(err=>{
-//         console.log(err);
-//       })
-//   })
-// }
+if(paymentStatusSections){
+  [...paymentStatusSections].forEach(item=>{
+    fetch(`/api/checkout/delivery-status/qr/${item.dataset.orderId}`).then(res=>res.json())
+      .then(data=>{
+        console.log(data);
+      }).catch(err=>{
+        console.log(err);
+      })
+  })
+}
 
-// const buttonQrHistorys = document.querySelectorAll(".button-qr-history");
-// if(buttonQrHistorys){
-//   buttonQrHistorys.forEach(button => {
-//     button.addEventListener("click", ()=>{
-//       const qrSection = button.parentElement.querySelector(".section-qr-history");
-//       qrSection.classList.toggle("d-none");
-//     })
-//   })
-// }
+const buttonQrHistorys = document.querySelectorAll(".button-qr-history");
+if(buttonQrHistorys){
+  buttonQrHistorys.forEach(button => {
+    button.addEventListener("click", (e)=>{
+      e.stopPropagation();
+      const qrSection = button.parentElement.querySelector(".section-qr-history");
+      qrSection.classList.toggle("d-none");
+    })
+  })
+}
 
 // history page
 const orderInfos = document.querySelectorAll(".orderInfo");
@@ -29,7 +30,16 @@ if(orderInfos){
     orderInfo.addEventListener("click", (e) => {
       const orderProduct = orderInfo.parentElement.querySelector(".orderProducts");
       orderProduct.classList.toggle("d-none");
+      orderInfo.classList.toggle("active");
     })
   })
 }
 // end - history page
+
+document.addEventListener("click", (e)=>{
+  const innerQrSection = document.querySelector(".inner-section-qr");
+  const qrSection = document.querySelector(".section-qr-history");
+  if(!qrSection.classList.contains("d-none") && !innerQrSection.contains(e.target)){
+    qrSection.classList.add("d-none");
+  }
+})
