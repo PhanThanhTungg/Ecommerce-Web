a376124c9a0abff424b4b14bdd3f79152c5f3398
git of tung <phantungpt03@gmail.com>
2025-04-16 15:19:59 +0700

    update ui

--- public/admin/js/script.js
+++ public/admin/js/script.js
@@ -280,7 +280,6 @@ const sort = document.querySelector("[sort]")
 if (sort) {
   let url = new URL(window.location.href)
   const sortSelect = sort.querySelector("[sort-select]")
-  const sortClear = sort.querySelector("[sort-clear")
 
   sortSelect.addEventListener("change", (e) => {
     const value = e.target.value
@@ -291,18 +290,11 @@ if (sort) {
 
     window.location.href = url.href
   })
-  // xoa sap xep
-  sortClear.addEventListener("click", () => {
-    url.searchParams.delete("sortKey")
-    url.searchParams.delete("sortValue")
-
-    window.location.href = url.href
-  })
 
   // them selected cho option
   const sortKey = url.searchParams.get("sortKey")
   const sortValue = url.searchParams.get("sortValue")
-
+  console.log(sortKey, sortValue);
   if (sortKey && sortValue) {
     const stringSort = `${sortKey}-${sortValue}`
     const optionSelected = document.querySelector(`option[value='${stringSort}']`)
