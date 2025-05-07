f6f2394e76085d621b4925809965b83a2d6ffa9c
PhanThanhTungg <phantungpt03@gmail.com>
2025-05-07 11:40:57 +0700

    Enhance OLAP fact sale query by adding dynamic HAVING conditions and restructuring SQL level definitions

--- controller/client/api/dashboard.controller.api.js
+++ controller/client/api/dashboard.controller.api.js
@@ -2,15 +2,33 @@ const Fact_sale = require("../../../DWH/modelWH/Fact_Sale.model");
 module.exports.olapFactSale = async (req, res) => {
   const body = req.body;
 
-  const time_level = body.timeRollUp == "year"?"t.year":
-  body.timeRollUp == "month"?"t.year, t.month":
-  body.timeRollUp == "day"?"t.year, t.month, t.day":"";
+  const levelArray = [];
+  const havingArray = [];
 
-  const location_level = body.locationRollUp == "province"?",l.province":
-  body.locationRollUp == "district"?",l.province,l.district":
-  body.locationRollUp == "commune"?",l.province,l.district,l.commune":"";
+  const time_level = body.timeRollUp == "year" ? "t.year" :
+    body.timeRollUp == "month" ? "t.year, t.month" :
+      body.timeRollUp == "day" ? "t.year, t.month, t.day" : "";
+  if(time_level !="") levelArray.push(time_level);
 
-  const sql_level = `${time_level}${location_level}`;
+  const Time_is_not_null =
+    body.timeRollUp == "year" ? "t.year IS NOT NULL" :
+      body.timeRollUp == "month" ? "t.year IS NOT NULL AND t.month IS NOT NULL" :
+        body.timeRollUp == "day" ? "t.year IS NOT NULL AND t.month IS NOT NULL AND t.day IS NOT NULL" : "";
+  if(Time_is_not_null !="") havingArray.push(Time_is_not_null);
+
+  const location_level = body.locationRollUp == "province" ? "l.province" :
+    body.locationRollUp == "district" ? "l.province,l.district" :
+      body.locationRollUp == "commune" ? "l.province,l.district,l.commune" : "";
+  if(location_level !="") levelArray.push(location_level);
+
+  const Location_is_not_null =
+    body.locationRollUp == "province" ? "l.province IS NOT NULL" :
+      body.locationRollUp == "district" ? "l.province IS NOT NULL AND l.district IS NOT NULL" :
+        body.locationRollUp == "commune" ? "l.province IS NOT NULL AND l.district IS NOT NULL AND l.commune IS NOT NULL" : "";
+  if(Location_is_not_null !="") havingArray.push(Location_is_not_null);
+
+  const sql_level = `${levelArray.join(",")}`;
+  const sql_having = `${havingArray.join(" AND ")}`;
 
   const [factSale] = await Fact_sale.sequelize.query(`
     SELECT 
@@ -23,13 +41,19 @@ module.exports.olapFactSale = async (req, res) => {
       JOIN Dim_Location l ON f.Location_key = l.Location_key
     GROUP BY 
       ROLLUP (${sql_level})
-    ORDER BY 
-      ${sql_level};
+    HAVING ${sql_having}
+    ORDER BY ${sql_level};
   `);
+
+  const keys = Object.keys(factSale[0]);
+  const resData = {};
+  keys.forEach((key) => {
+    resData[key] = factSale.map((item) => item[key]);
+  });
 
   res.status(200).json({
     status: "success",
-    data: factSale
+    data: resData
   });
 
 
