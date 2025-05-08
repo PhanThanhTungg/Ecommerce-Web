01b7070cde7d44450cb0d806e0f918c83edf7727
PhanThanhTungg <phantungpt03@gmail.com>
2025-05-08 10:20:49 +0700

    Add time dice functionality and enhance rollup handling in OLAP fact sale query

--- controller/client/api/dashboard.controller.api.js
+++ controller/client/api/dashboard.controller.api.js
@@ -5,6 +5,7 @@ module.exports.olapFactSale = async (req, res) => {
   const levelArray = [];
   const havingArray = [];
 
+  //Time rollup 
   const time_level = body.timeRollUp == "year" ? "t.year" :
     body.timeRollUp == "month" ? "t.year, t.month" :
       body.timeRollUp == "day" ? "t.year, t.month, t.day" : "";
@@ -16,6 +17,37 @@ module.exports.olapFactSale = async (req, res) => {
         body.timeRollUp == "day" ? "t.year IS NOT NULL AND t.month IS NOT NULL AND t.day IS NOT NULL" : "";
   if (Time_is_not_null != "") havingArray.push(Time_is_not_null);
 
+  // Time dice
+  const timeWhereConditions = [];
+  if (body.timeDice) {
+    const timeDiceStartSplit = body.timeDice.start.split("-");
+    const timeDiceEndSplit = body.timeDice.end.split("-");
+    if (timeDiceStartSplit.length != timeDiceEndSplit.length) {
+      return res.status(400).json({
+        status: "error",
+        message: "Invalid time dice format"
+      });
+    }
+    if (timeDiceStartSplit.length == 1) {
+      const startYear = parseInt(timeDiceStartSplit[0]);
+      const endYear = parseInt(timeDiceEndSplit[0]);
+      timeWhereConditions.push(`t.year BETWEEN ${startYear} AND ${endYear}`);
+      console.log(timeWhereConditions)
+    }
+    else if (timeDiceStartSplit.length == 2) {
+      const [startMonth, startYear] = timeDiceStartSplit.map(Number);
+      const [endMonth, endYear] = timeDiceEndSplit.map(Number);
+      timeWhereConditions.push(`(t.year > ${startYear} OR (t.year = ${startYear} AND t.month >= ${startMonth}))`);
+      timeWhereConditions.push(`(t.year < ${endYear} OR (t.year = ${endYear} AND t.month <= ${endMonth}))`);
+    }
+    else if (timeDiceStartSplit.length == 3) {
+      const startDateConvert = timeDiceStartSplit.reverse().join("-");
+      const endDateConvert = timeDiceEndSplit.reverse().join("-");
+      timeWhereConditions.push(`CONVERT(date, CONCAT(t.year, '-', t.month, '-', t.day)) BETWEEN '${startDateConvert}' AND '${endDateConvert}'`);
+    }
+  }
+
+  //location rollup
   const location_level = body.locationRollUp == "province" ? "l.province" :
     body.locationRollUp == "district" ? "l.province,l.district" :
       body.locationRollUp == "commune" ? "l.province,l.district,l.commune" : "";
@@ -27,6 +59,7 @@ module.exports.olapFactSale = async (req, res) => {
         body.locationRollUp == "commune" ? "l.province IS NOT NULL AND l.district IS NOT NULL AND l.commune IS NOT NULL" : "";
   if (Location_is_not_null != "") havingArray.push(Location_is_not_null);
 
+  //product rollup
   const product_level =
     body.productRollUp == "category" ? "c.category_key, c.category_name" :
       body.productRollUp == "product" ? "p.product_key, p.product_name" : "";
@@ -37,18 +70,21 @@ module.exports.olapFactSale = async (req, res) => {
       body.productRollUp == "product" ? "p.product_key IS NOT NULL" : "";
   if (Product_is_not_null !== "") havingArray.push(Product_is_not_null);
 
-  const customer_level = 
+  //customer rollup
+  const customer_level =
     body.customer == "gender" ? "cu.Gender, cu.Type" :
-    body.customer == "type" ? "cu.Type, cu.Gender" : "";
+      body.customer == "type" ? "cu.Type, cu.Gender" : "";
   if (customer_level !== "") levelArray.push(customer_level);
 
-  const Customer_is_not_null = 
+  const Customer_is_not_null =
     body.customer == "gender" ? "cu.Gender IS NOT NULL and cu.Type is not null" :
-    body.customer == "type" ? "cu.Type IS NOT NULL and cu.Gender is not null" : "";
+      body.customer == "type" ? "cu.Type IS NOT NULL and cu.Gender is not null" : "";
   if (Customer_is_not_null !== "") havingArray.push(Customer_is_not_null);
 
   const sql_level = `${levelArray.join(",")}`;
   const sql_having = `${havingArray.join(" AND ")}`;
+  const sql_where = timeWhereConditions.length > 0
+    ? `WHERE ${timeWhereConditions.join(' AND ')}` : '';
 
   const [factSale] = await Fact_sale.sequelize.query(`
     SELECT 
@@ -62,13 +98,14 @@ module.exports.olapFactSale = async (req, res) => {
       JOIN Dim_Product p ON f.Product_key = p.Product_key
       JOIN Dim_Category c ON p.Category_key = c.Category_key
       JOIN Dim_Customer cu ON f.Customer_key = cu.Customer_key
+    ${sql_where}
     GROUP BY 
       ROLLUP (${sql_level})
     HAVING ${sql_having}
     ORDER BY ${sql_level};
   `);
-  console.log(factSale);
-  const keys = Object.keys(factSale[0]);
+
+  const keys = Object.keys(factSale[0] || {});
   const resData = {};
   keys.forEach((key) => {
     resData[key] = factSale.map((item) => item[key]);
