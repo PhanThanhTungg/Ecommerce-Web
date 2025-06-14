f14f964e536a7fa3a07d47d2329d1249067b2fe0
PhanThanhTungg <166277205+PhanThanhTungg@users.noreply.github.com>
2025-06-14 21:35:35 +0700

    chore: update bcrypt dependency to version 6.0.0

--- DWH/connectToMSSQL.js
+++ DWH/connectToMSSQL.js
@@ -1,23 +1,21 @@
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize('WHHTTTQL', 'sa', '123456', {
+const sequelize = new Sequelize('WHHTTTQL', 'sa', process.env.MSSQL_SA_PASSWORD, {
   dialect: 'mssql',
   host: process.env.MSSQL_HOST,
   port: process.env.MSSQL_PORT,
   dialectOptions: {
     options: {
-      // instanceName: 'SERVER_HANOI',
       encrypt: false,
       trustServerCertificate: true,
     }
   }
-});
+}); 
 
 sequelize.authenticate().then(() => {
-    console.log('Connection mssql has been established successfully.');
+  console.log('Connection mssql has been established successfully.');
 }).catch((error) => {
-    console.error('Unable to connect mssql to the database: ', error);
+  console.error('Unable to connect mssql to the database: ', error);
 });
-
 
 module.exports = sequelize;
